6f48719ce9cd5fe4aa56e969d9a3b550778773e7
Asliddin <100646459+asliddinxanov@users.noreply.github.com>
2022-06-01 10:16:06 +0900

    create parse arguments

--- mnist3.py
+++ mnist3.py
@@ -45,3 +45,47 @@ def checkargs():
     # Gloval vaiables (too many return values...)
     global INETWORK, IEPOCH, ISEED, RATIO, IGRAPHIC, OUTDIR, INDIR, ILOAD
 
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description='MNIST Sample Program')
+    parser.add_argument('-n','--INETWORK', type=int,   help='NEURAL NETWORK (0:Single Dense Layer / 1:CNN / 2:User Defined)', choices=[0,1,2])
+    parser.add_argument('-e','--IEPOCH',   type=int,   help='Number of epochs (Training)')
+    parser.add_argument('-r','--RATIO',    type=float, help='Ratio of validation/(training+validation) [0.0-1.0]')
+    parser.add_argument('-s','--ISEED',    type=int,   help='Seed of pseudo random number')
+    parser.add_argument('-g','--IGRAPHIC', type=int,   help='Graphic Display (0:Only File-output / 1:Display Window & File-output)', choices=[0,1])
+    parser.add_argument('-o','--OUTDIR',               help='Result Directory')
+    parser.add_argument('-i','--INDIR',                help='Input Directory')
+    parser.add_argument('-l','--ILOAD',                help='Use PreTrained Weights/bias')
+    args = parser.parse_args()
+
+    if args.INETWORK != None:
+        INETWORK = args.INETWORK
+    if args.IEPOCH   != None:
+        IEPOCH   = args.IEPOCH
+    if args.RATIO    != None:
+        if args.RATIO >= 0.0  and args.RATIO <=1.0:
+            RATIO    = args.RATIO
+        else:
+            print("[ERROR] RATIO must be range from 0.0 to 1.0.")
+            exit
+    if args.ISEED    != None:
+        ISEED    = args.ISEED
+    if args.IGRAPHIC != None:
+        IGRAPHIC = args.IGRAPHIC
+    if args.OUTDIR   != None:
+        OUTDIR   = args.OUTDIR
+    if args.INDIR   != None:
+        INDIR    = args.INDIR
+    if args.ILOAD   != None:
+        ILOAD    = args.ILOAD
+
+    ### Print Settings ###
+    print("INETWORK =",INETWORK)
+    print("IEPOCH   =",IEPOCH)
+    print("ISEED    =",ISEED)
+    print("RATIO    =",RATIO)
+    print("IGRAPHIC =",IGRAPHIC)
+    print("OUTDIR   =",OUTDIR)
+    print("INDIR    =",INDIR)
+    print("ILOAD    =",ILOAD)
+
+    return
